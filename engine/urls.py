646f5bd5332b13c3ead78c4082e12bce0b7486ca
rustem <ragamyradow22@gmail.com>
2023-05-09 18:08:27 +0500

    login view

--- engine/urls.py
+++ engine/urls.py
@@ -4,7 +4,23 @@ app_name = 'engine'
 
 
 urlpatterns = [
+    path('login', views.loginView, name='loginurls'),
     path('', views.homeView, name='homeURL'),
-    path('/teachers', views.teachersView, name='teachersURL'),
+    path('teachers', views.teachersView, name='teachersURL'),
+    path('students', views.studentsView, name='studentsURL'),
+    path('addstudent', views.addstudentView, name='addstudentURL'),
+    path('editstudent', views.editstudentView, name='editstudentURL'),
+    path('aboutstudent', views.aboutstudentView, name='aboutstudentURL'),
+    path('calendar', views.calendarView, name='calendarURL'),
+    path('setting', views.settingView, name='settingURL'),
+    path('changep', views.changepView, name='changepURL'),
+    path('blog', views.blogView, name='blogURL'),
+    path('blogview', views.blogviewView, name='blogviewURL'),
+    path('addblog', views.addblogView, name='addblogURL'),
+    path('editblog', views.editblogView, name='editblogURL'),
+    path('chat', views.chatView, name='chatURL'),
+    path('myprofile', views.myprofileView, name='myprofileURL'),
+    path('editpro', views.editproView, name='editproURL'),
+    path('ssetting', views.ssettingView, name='ssettingURL'),
+    #path('/home', views.homeView, name='homeURL'),
 ]
-
--- engine/views.py
+++ engine/views.py
@@ -1,9 +1,80 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
+
+@login_required(login_url='engine:loginurls')
 def homeView(request):
     return render(request, 'engine/dashboard.html') 
 
 
-
+@login_required(login_url='engine:loginurls')
 def teachersView(request):
     return render(request, 'engine/teachers.html')
+
+def studentsView(request):
+    return render(request, 'engine/students.html')
+
+def addstudentView(request):
+    return render(request, 'engine/addstudent.html')
+
+
+def editstudentView(request):
+    return render(request, 'engine/editstudent.html')
+
+
+def aboutstudentView(request):
+    return render(request, 'engine/aboutstudent.html')
+
+
+def calendarView(request):
+    return render(request, 'engine/calendar.html')
+
+def settingView(request):
+    return render(request, 'engine/setting.html')
+
+
+def changepView(request):
+    return render(request, 'engine/changep.html')
+
+
+def blogView(request):
+    return render(request, 'engine/blog.html')
+
+
+def blogviewView(request):
+    return render(request, 'engine/blogview.html')
+
+
+def addblogView(request):
+    return render(request, 'engine/addblog.html')
+
+
+def editblogView(request):
+    return render(request, 'engine/editblog.html')
+
+
+def chatView(request):
+    return render(request, 'engine/chat.html')
+
+
+def myprofileView(request):
+    return render(request, 'engine/myprofile.html')
+
+
+def editproView(request):
+    return render(request, 'engine/editpro.html')
+
+
+def ssettingView(request):
+    return render(request, 'engine/ssetting.html')
+
+
+def loginView( request):
+    
+    return render(request, 'engine/login.html')
+
+
+def homeView(request):
+    return render(request, 'engine/home.html')
+
